6acabbb6fb8ee077a756335f5cb1a91ee27fac00
Ouadi Belmokhtar <ouadi.belmokhtar@gmail.com>
2019-07-25 10:48:18 +0100

    typo

--- src/chap8/mockexam/Tests7.java
+++ src/chap8/mockexam/Tests7.java
@@ -30,11 +30,18 @@ public class Tests7 {
 		int i2 = 1____3; // OK
 		System.out.println(i2); // prints 13
 
-		
 		int i5 = 10;
 		// The method println(boolean) in the type PrintStream
 		// is not applicable for the arguments (void)
-		//System.out.println(i < 20 ? out1() : out2());
+		// System.out.println(i < 20 ? out1() : out2());
+
+		StringBuilder sb = new StringBuilder("12345678");
+		sb.setLength(5);
+		sb.setLength(10);
+		System.out.println(sb.length()); // prints 10
+		System.out.println(sb);// prints "12345     " 5 spaces inside
+		
+		System.out.println("blooper".substring);
 	}
 
 	private static void out2() {
